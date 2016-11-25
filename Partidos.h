106c62f0203d700ea4f47dcec7d0ecc9df1273a4
Harold Mendoza <hrmendoza96@unitec.edu>
2016-11-25 17:10:33 -0600

    Commit sin errores

--- Partidos.h
+++ Partidos.h
@@ -7,27 +7,27 @@ using namespace std;
 
 class Partidos{
   private:
-    Equipos equipo1;
-    Equipos equipo2;
+    Equipos* equipo1;
+    Equipos* equipo2;
     int MarcadorEquipo1;
     int MarcadorEquipo2;
-    Equipos ganador;
-    Equipos local;
+    Equipos* ganador;
+    Equipos* local;
 
   public:
     Partidos();
-    Partidos(Equipos,Equipos,int,int,Equipos,Equipos);
-    Equipos getEquipo1();
-    Equipos getEquipo2();
-    Equipos getGanador();
-    Equipos getLocal();
+    Partidos(Equipos*,Equipos*,int,int,Equipos*);
+    Equipos* getEquipo1();
+    Equipos* getEquipo2();
+    Equipos* getGanador();
+    Equipos* getLocal();
     int getMarcadorEquipo1();
     int getMarcadorEquipo2();
 
-    void setEquipo1(Equipos);
-    void setEquipo2(Equipos);
-    void setGanador(Equipos);
-    void setLocal(Equipos);
+    void setEquipo1(Equipos*);
+    void setEquipo2(Equipos*);
+    void setGanador(Equipos*);
+    void setLocal(Equipos*);
     void setMarcadorEquipo1(int);
     void setMarcadorEquipo2(int);
 
