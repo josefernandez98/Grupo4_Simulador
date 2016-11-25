5107ad0309277ef3921a66847503f9faa563fa94
Jose Fernandez <jrfo_04@unitec.edu>
2016-11-25 14:29:16 -0600

    main

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "Entrenadores.h"
+#include "Equipos.h"
+#include "Jugadores.h"
+#include "Partidos.h"
+#include "Personas.h"
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}//Fin del main
